4e5fd81deb479fbc6f0fb3716621884d4ea6fb95
Shamp00 <lucas.laplanche77@gmail.com>
2026-05-07 16:58:26 +0200

    updated leaderboard script to fetch user's steam id

--- _scripts/generate_json.php
+++ _scripts/generate_json.php
@@ -1,12 +1,29 @@
 <?php
+require_once __DIR__ . '/../_inc/config.php';
+require_once __DIR__ . '/../_inc/functions.php';
+
 $db = new PDO('sqlite:' . __DIR__ . '/stats.db');
 $query = $db->query("SELECT 
                         COALESCE(p.display_name, p.name) AS name, 
-                        p.avatar, 
+                        p.avatar,
+                        p.steamid, 
                         s.count 
                      FROM player_stats s 
                      JOIN players_info p ON s.steamid = p.steamid 
                      ORDER BY s.count DESC LIMIT 18");
 
-$results = $query->fetchAll(PDO::FETCH_ASSOC);
-file_put_contents(__DIR__ . '/leaderboard_cache.json', json_encode($results));
+$rows = $query->fetchAll(PDO::FETCH_ASSOC);
+$final_results = [];
+
+foreach ($rows as $row) {
+   $id64 = steamID3To64($row['steamid']);
+
+   $final_results[] = [
+       'name' => $row['name'],
+       'avatar' => $row['avatar'],
+       'steamid' => $id64,
+       'count' => $row['count']
+   ];
+}
+
+file_put_contents(__DIR__ . '/leaderboard_cache.json', json_encode($final_results));
