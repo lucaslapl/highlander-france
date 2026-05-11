4199864d15e0ad901f9681b93b73b06a1a97ba04
Shamp00 <lucas.laplanche77@gmail.com>
2026-05-11 12:19:14 +0200

    removed file extension in url

--- _inc/header.php
+++ _inc/header.php
@@ -21,18 +21,18 @@
                     ?>
 
                     <ul class="flex justify-center align-center">
-                        <li><a href="/index.php" class="<?= ($page_actuelle == 'index.php') ? 'active' : '' ?>">Accueil</a></li>
-                        <li><a href="/staff.php" class="<?= ($page_actuelle == 'staff.php') ? 'active' : '' ?>">L'équipe</a></li>
-                        <li><a href="/hall-of-fame.php" class="<?= ($page_actuelle == 'hall-of-fame.php') ? 'active' : '' ?>">Hall of Fame</a></li>
-                        <li><a href="/match-logs.php" class="<?= ($page_actuelle == 'match-logs.php') ? 'active' : '' ?>">Match Stats</a></li>
+                        <li><a href="/index" class="<?= ($page_actuelle == 'index.php') ? 'active' : '' ?>">Accueil</a></li>
+                        <li><a href="/staff" class="<?= ($page_actuelle == 'staff.php') ? 'active' : '' ?>">L'équipe</a></li>
+                        <li><a href="/hall-of-fame" class="<?= ($page_actuelle == 'hall-of-fame.php') ? 'active' : '' ?>">Hall of Fame</a></li>
+                        <li><a href="/match-logs" class="<?= ($page_actuelle == 'match-logs.php') ? 'active' : '' ?>">Match Stats</a></li>
                     </ul>
                     <div class="nav-right flex justify-center align-center">
                         <div id="session-profile" class="flex justify-center align-center">
                             <?php if (isset($_SESSION['steamid'])): ?>
-                                <a href="/profile/dashboard.php">Mon Profil</a>
-                                <a href="/logout.php">Déconnexion</a>
+                                <a href="/profile/dashboard">Mon Profil</a>
+                                <a href="/logout">Déconnexion</a>
                             <?php else: ?>
-                                <a href="/login.php">
+                                <a href="/login">
                                     <img class="steamlogin" src="/_img/sits_01.png" alt="Connexion via Steam">
                                 </a>
                             <?php endif; ?>
