1c7f17965f85ffdc2c534df47ce574afe87b5588
Shamp00 <lucas.laplanche77@gmail.com>
2026-05-11 15:39:48 +0200

    moved leaderboard functions to js file

--- hall-of-fame.php
+++ hall-of-fame.php
@@ -85,6 +85,7 @@ require_once "_inc/functions.php";
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://kit.fontawesome.com/2f306d349c.js" crossorigin="anonymous"></script>
+<script src="_js/main.js"></script>
 <script>
     window.addEventListener("load", function () {
 
@@ -122,45 +123,6 @@ require_once "_inc/functions.php";
 
     }, 300);
 });
-
-async function loadLeaderboard() {
-    const tbody = document.getElementById('leaderboard-body');
-    
-    try {
-        const response = await fetch('_scripts/leaderboard_cache.json?v=' + new Date().getTime()); // Le ?v=... évite le cache navigateur
-        const players = await response.json();
-        
-        tbody.innerHTML = ''; // Nettoyer avant affichage
-        
-        players.forEach((player, index) => {
-            const row = document.createElement('tr');
-            
-            row.innerHTML = `
-                <td>#${index + 1}</td>
-                <td>
-                    <div class="player-info">
-                        <a href="/profile/profil?steamid=${player.steamid}" class="player-link">
-                            <img src="${player.avatar}" class="player-avatar" alt="Avatar de ${escapeHtml(player.name)}">
-                            <span>${escapeHtml(player.name)}</span>
-                        </a>
-                    </div>
-                </td>
-                <td>${player.count}</td>
-            `;
-            tbody.appendChild(row);
-        });
-    } catch (error) {
-        tbody.innerHTML = '<tr><td colspan="3">Erreur lors du chargement...</td></tr>';
-        console.error('Erreur:', error);
-    }
-}
-
-// Sécurité basique pour éviter les caractères spéciaux dans les pseudos
-function escapeHtml(text) {
-    const div = document.createElement('div');
-    div.textContent = text;
-    return div.innerHTML;
-}
 
 // Lancement au chargement de la page
 loadLeaderboard();
