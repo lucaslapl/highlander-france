bc76fe294a8f7c06e0615a6dcfe96334be2af7fa
Shamp00 <lucas.laplanche77@gmail.com>
2026-05-21 07:59:39 +0200

    put longer session duration (30 days)

--- _inc/config.php
+++ _inc/config.php
@@ -1,4 +1,15 @@
 <?php
+$durations_session = 30 * 24 * 60; // 30 jours en secondes
+session_set_cookie_params([
+    'lifetime' => $durations_session,
+    'path' => '/',
+    'domain' => $_SERVER['HTTP_HOST'],
+    'secure' => true,
+    'httponly' => true,
+    'samesite' => 'Lax'
+]); 
+ini_set('session.gc_maxlifetime', $durations_session);
+
 if (php_sapi_name() !== 'cli') {
     if (session_status() === PHP_SESSION_NONE) {
         session_start();
